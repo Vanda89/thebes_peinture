90ad6f6aa3733f0a0a2cce0aeeb461e3563682ca
arknoid80 <arknoid80@vps589304.ovh.net>
2019-02-25 08:55:09 +0100

    Initialisation

---
+++ content/themes/thebes_peinture/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
+}
+
+add_action('wp_enqueue_scripts', 'style_theme_enfant', 20);
+function style_theme_enfant()
+{
+    wp_dequeue_style('one-page-express-style', get_stylesheet_uri());
+    wp_enqueue_style('enfant-style', get_stylesheet_uri());
+}
